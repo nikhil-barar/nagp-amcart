9d85eccda2ad60c10d08f90258b6b3613d55d613
nikhilbarar <nikhil.barar@nagarro.com>
2017-10-10 03:41:01 +0530

    Adding Elasticsearch Framework

---
+++ amcart-parent/src/com/nagarro/amcart/config/ElasticsearchConfiguration.java
@@ -0,0 +1,35 @@
+package com.nagarro.amcart.config;
+
+import javax.annotation.Resource;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.nagarro.amcart.search")
+public class ElasticsearchConfiguration {
+	@Resource
+	private Environment environment;
+
+	@Bean
+	public Client client() {
+		TransportClient client = new TransportClient();
+		TransportAddress address = new InetSocketTransportAddress(environment.getProperty("elasticsearch.host"),
+				Integer.parseInt(environment.getProperty("elasticsearch.port")));
+		client.addTransportAddress(address);
+		return client;
+	}
+
+	@Bean
+	public ElasticsearchOperations elasticsearchTemplate() {
+		return new ElasticsearchTemplate(client());
+	}
+}
