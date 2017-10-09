260cfd128197222fd08526fae805d06ccb895d72
mandeepjolly <mandeep.kaur.jolly@gmail.com>
2017-10-09 19:54:46 +0530

    email services initial commit
    email services initial commit

---
+++ amcart-services/src/com/nagarro/amcart/email/EmailAttachmentFile.java
@@ -0,0 +1,29 @@
+package com.nagarro.amcart.email;
+
+public class EmailAttachmentFile {
+	
+	private String path;
+	private String name;
+	private String description;
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
