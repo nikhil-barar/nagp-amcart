665e14016863e1bea0cd25db46c82accc84aced3
Arjun Duggal <arjun.duggal@nagarro.com>
2017-11-02 23:18:30 +0530

    Moved the App.java class to amway-web with relevant configuration files, changed the amway-web project structure and a sample jsp framework added.

---
+++ amcart-web/src/main/java/com/nagarro/amcart/web/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.nagarro.amcart.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+   @RequestMapping("/")
+   public String index() {
+      return "index";
+   }
+
+   @RequestMapping(value = "/hello",  method = RequestMethod.POST)
+   public String sayHello(@RequestParam String name, Model model) {
+      model.addAttribute("name", name);
+      return "hello";
+   }
+}
