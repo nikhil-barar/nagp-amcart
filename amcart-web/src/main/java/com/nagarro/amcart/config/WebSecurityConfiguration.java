63982399de67a86afe27b9a6fee401c97bfd9aac
Naresh Goyal <naresh.goyal@nagarro.com>
2017-11-20 21:39:34 +0530

    Permitted ROLE_CUSTOMER in security config. New user have this role ROLE_CUSTOMER.

--- amcart-web/src/main/java/com/nagarro/amcart/config/WebSecurityConfiguration.java
+++ amcart-web/src/main/java/com/nagarro/amcart/config/WebSecurityConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -39,7 +38,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(permittedUrls).permitAll()
-                .antMatchers(adminPermittedUrls).hasAnyAuthority(RoleType.ROLE_ADMIN.getName())
+                .antMatchers(adminPermittedUrls).hasAnyAuthority(RoleType.ROLE_ADMIN.getName(), RoleType.ROLE_CUSTOMER.getName())
                 .anyRequest().authenticated().and().formLogin().loginPage(loginUrl).permitAll().and().logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher(logoutUrl)).logoutSuccessUrl(logoutSuccessUrl);
     }
